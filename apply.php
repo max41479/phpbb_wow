3bb09e7d65fd9620fd300bfa554de16bdec466cc
max41479 <cod41479@list.ru>
2013-03-30 16:08:31 +0400

    excess guild name deleted

--- apply.php
+++ apply.php
@@ -629,7 +629,7 @@ function build_candidate(dkp_character &$candidate, apply_post &$apply_post )
 			$candidate->wowprogressurl = sprintf('http://www.wowprogress.com/character/%s/', $candidate->region) . rawurlencode(str_replace(" ", "-", $candidate->realm)). '/' . urlencode($candidate->name);
 			$candidate->wowheroesurl = sprintf('http://www.wow-heroes.com/character/%s/', $candidate->region) . rawurlencode($candidate->realm). '/' . urlencode($candidate->name) . '/';
 			//guild
-			$candidate->guild = $blizzard['guild']['name'] . '@' . $candidate->realm;
+			$candidate->guild = $blizzard['guild']['name'];
 
 			//gear
 			$candidate->averageItemLevel = $blizzard['items']['averageItemLevel'];
