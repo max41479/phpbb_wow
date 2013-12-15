c34d1f11cdafaaa08fdf0edb321df0c95b065e58
max41479 <cod41479@gmail.com>
2013-12-15 20:06:46 +0400

    http://pbwow.com/forum/viewtopic.php?f=3&t=11304&start=10#p20694 xmas theme added and some translation of pbwow acp

--- language/ru/mods/info_acp_pbwow.php
+++ language/ru/mods/info_acp_pbwow.php
@@ -12,26 +12,26 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	// old PBWoW
 	'ACP_PBWOW_INFO'			=> 'PBWoW',
-	'ACP_PBWOW_INDEX_INFO'		=> 'Index',
-	'ACP_PBWOW_CONFIG_INFO'		=> 'Configuration',
-	'ACP_PBWOW_ADS_INFO'		=> 'Advertisements',
+	'ACP_PBWOW_INDEX_INFO'		=> 'Главная',
+	'ACP_PBWOW_CONFIG_INFO'		=> 'Конфигурация',
+	'ACP_PBWOW_ADS_INFO'		=> 'Реклама',
 
 	// PBWoW 2
 	'ACP_PBWOW2_CATEGORY'		=> 'PBWoW 2',
-	'ACP_PBWOW2_OVERVIEW'		=> 'Overview',
-	'ACP_PBWOW2_CONFIG'			=> 'Configuration',
-	'ACP_PBWOW2_POSTSTYLING'	=> 'Post Styling',
-	'ACP_PBWOW2_ADS'			=> 'Advertisements',
+	'ACP_PBWOW2_OVERVIEW'		=> 'Обзор',
+	'ACP_PBWOW2_CONFIG'			=> 'Конфигурация',
+	'ACP_PBWOW2_POSTSTYLING'	=> 'Стиль сообщений',
+	'ACP_PBWOW2_ADS'			=> 'Реклама',
 
-	'LOG_PBWOW_CONFIG'			=> '<strong>Altered PBWoW settings</strong><br />&raquo; %s',
+	'LOG_PBWOW_CONFIG'			=> '<strong>Изменены настройки PBWoW</strong><br />&raquo; %s',
 	
 	// Installer
-	'INSTALL_PBWOW2_MOD'			=> 'Install PBWoW 2 MOD',
-	'INSTALL_PBWOW2_MOD_CONFIRM'	=> 'Are you sure you want to install the PBWoW 2 MOD?',
-	'UPDATE_PBWOW2_MOD'				=> 'Update PBWoW 2 MOD',
-	'UPDATE_PBWOW2_MOD_CONFIRM'		=> 'Are you sure you want to update the PBWoW 2 MOD?',
-	'UNINSTALL_PBWOW2_MOD'			=> 'Uninstall PBWoW 2 MOD',
-	'UNINSTALL_PBWOW2_MOD_CONFIRM'	=> 'Are you sure you want to uninstall the PBWoW 2 MOD?',
+	'INSTALL_PBWOW2_MOD'			=> 'Установить PBWoW 2 MOD',
+	'INSTALL_PBWOW2_MOD_CONFIRM'	=> 'Вы уверены, что хотите установить PBWoW 2 MOD?',
+	'UPDATE_PBWOW2_MOD'				=> 'Обновить PBWoW 2 MOD',
+	'UPDATE_PBWOW2_MOD_CONFIRM'		=> 'Вы уверены, что хотите обновить PBWoW 2 MOD?',
+	'UNINSTALL_PBWOW2_MOD'			=> 'Удалить PBWoW 2 MOD',
+	'UNINSTALL_PBWOW2_MOD_CONFIRM'	=> 'Вы уверены, что хотите удалить PBWoW 2 MOD?',
 ));
 
 ?>
